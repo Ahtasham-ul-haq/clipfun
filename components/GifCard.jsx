fb353fd6da4b68520ab6613b3d006464867c73e6
Ahtasham-ul-Haq <ahtasham1802@gmail.com>
2024-11-10 16:01:40 +0500

    fetch API

---
+++ components/GifCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GifCard = ({gif}) => {
+  return (
+      <div className={"border border-cyan-500 w-[200px] h-[400px] justify-center self-center shadow-lg"}>
+        <img src={gif.images.original.url} alt={gif.title} className={"object-contain"}/>
+        <h3>{gif.title}</h3>
+
+      </div>
+
+  )
+}
+export default GifCard
